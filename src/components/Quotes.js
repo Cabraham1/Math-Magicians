05110afaa4b882bad8137d4036af7edf240a218c
cabraham1 <christopherabraham8@gmail.com>
2023-03-24 21:58:57 +0100

    get Quotes using Useeffect

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,30 +1,29 @@
-/* eslint-disable */
-import React, { useState, useEffect, useRef } from "react";
-import "./Quotes.css"
+import React, { useState, useEffect, useRef } from 'react';
+import './Quotes.css';
 
 const Quote = () => {
-  const [quotes, setQuotes] = useState("");
+  const [quotes, setQuotes] = useState('Loading...');
   const textRef = useRef();
-  const colors = ["#ffff00", "#90ee90", "#ffa500", "#ff68ff", "#a9a9e7"];
-  const category = "happiness";
-  const apiKey = "8vBF9RpWFlSYRkxXze3U/A==bR662D4w38AWnlA5";
+  const colors = ['#ffff00', '#90ee90', '#ffa500', '#ff68ff', '#a9a9e7'];
 
   const getQuotes = () => {
+    const category = 'happiness';
+    const apiKey = '8vBF9RpWFlSYRkxXze3U/A==bR662D4w38AWnlA5';
     fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-      method: "GET",
-      headers: { "X-Api-Key": apiKey },
-      contentType: "application/json",
+      method: 'GET',
+      headers: { 'X-Api-Key': apiKey },
+      contentType: 'application/json',
     }).then((res) => res.json())
       .then((data) => {
         setQuotes(data[0]);
       });
-    
   };
+  useEffect(() => {
     getQuotes();
+  }, []);
 
   useEffect(() => {
-    textRef.current.style.color =
-      colors[Math.floor(Math.random() * colors.length)];
+    textRef.current.style.color = colors[Math.floor(Math.random() * colors.length)];
   }, [quotes]);
   return (
     <>
@@ -32,8 +31,7 @@ const Quote = () => {
         <h1>{quotes[0]}</h1>
         <p ref={textRef}>{quotes.quote}</p>
         <p className="author">{quotes.author}</p>
-        <div>        
-        </div>
+        <div />
       </div>
     </>
   );
