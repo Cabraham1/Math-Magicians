500e6451b718c218d69a97d08c79a4d901359977
cabraham1 <christopherabraham8@gmail.com>
2023-03-24 13:26:16 +0100

    Install react dom to render elememt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+        </Routes>
+      </BrowserRouter>
       <Calculator />
     </div>
   );
